ac3b7da7d1278a182a2c2ecd98cc08ac725dcb7d
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2022-07-28 02:06:54 +0200

    section 10 is completed

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Categrory from "../category/category.component";
 
 import "./shop.styles.scss";
 
@@ -8,6 +9,7 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Categrory />} />
     </Routes>
   );
 };
