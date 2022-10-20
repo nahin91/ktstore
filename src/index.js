6f4a29fd71788c93c90bcfa843c5acd3d171460f
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2022-10-20 17:35:16 +0200

    added .env file with secrete stripe key

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react'
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { persistor, store } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
@@ -15,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -126,3 +126,4 @@ export function* userSagas() {
         call(onSignOutStart),
     ]);
 }
+
