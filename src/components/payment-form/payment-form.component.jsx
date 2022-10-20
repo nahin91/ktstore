08d085da3ea0a7a256510b8c631873d0e5cf7d63
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2022-10-21 01:02:10 +0200

    serverless function for Netlify is created

---
+++ src/components/payment-form/payment-form.component.jsx
@@ -0,0 +1,32 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+
+import { FormContainer, PaymentFormContainer } from "./payment-form-styles";
+
+const PaymentForm = () => {
+    const stripe = useStripe()
+    const elements = useElements()
+
+
+    const paymentHandler = async (e) => {
+        e.preventDefault()
+
+        if(!stripe || !elements) return;
+
+        
+
+    }
+
+  return (
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2>Credit Card Payment: </h2>
+        <CardElement />
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay Now </Button>
+      </FormContainer>
+    </PaymentFormContainer>
+  );
+};
+
+export default PaymentForm;
