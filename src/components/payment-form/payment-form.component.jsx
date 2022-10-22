0681e25f4d6ebffc8e125099202089c62dbc700f
Mohammad Nahin Islam <sec.tabrez03@gmail.com>
2022-10-22 10:20:20 +0200

    dummy hard coded payment is checked

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,26 +1,50 @@
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import { FormContainer, PaymentFormContainer } from "./payment-form-styles";
 
 const PaymentForm = () => {
-    const stripe = useStripe()
-    const elements = useElements()
+  const stripe = useStripe();
+  const elements = useElements();
 
+  const paymentHandler = async (e) => {
+    e.preventDefault();
 
-    const paymentHandler = async (e) => {
-        e.preventDefault()
+    if (!stripe || !elements) return;
 
-        if(!stripe || !elements) return;
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
 
-        
+    const {
+      paymentIntent: { client_secret },
+    } = response;
 
+    console.log(client_secret);
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Nahin",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Success");
+      }
     }
+  };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay Now </Button>
